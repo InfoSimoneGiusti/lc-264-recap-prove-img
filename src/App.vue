2d85f64d3a59113b95733e8846aeaa84a5294f61
Simone Giusti <info@simonegiusti.it>
2023-03-28 13:14:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Ciao Mondo</h1>
+    <img class="immagine" v-for="(immagine, index) in immagini" :src="getImageUrl(immagine)" :key="`immagine-${index}`" />
+
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        immagini: [
+          'immagine.jpg',
+          'immagine_2.jpg'
+        ]
+      }
+    },
+    methods: {
+      getImageUrl(name) {
+        return new URL(`./assets/${name}`, import.meta.url).href;
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss">
+  @use './styles/general.scss' as *;
+
+  //se mi riferisco ad una immagine qua, ad esempio per una background-image, devo riferirmi alla path relativa
+
+
+</style>
